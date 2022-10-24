60a1e84acd717ff5a80a926b76af38ee55a29f5a
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-10-24 22:57:56 +0300

    Add files via upload

--- E01. Database Access With JDBC/Enums/Constants.java
+++ E01. Database Access With JDBC/Enums/Constants.java
@@ -6,9 +6,11 @@ public enum Constants {
     static final String USER_VALUE = "root";
 
     static final String PASSWORD_KEY = "password";
-    static final String PASWORD_VALUE = "Saymellon#1034";
+    static final String PASSWORD_VALUE = "1234";
 
-    static final String MINOINS_DB_URL = "jdbc:mysql://localhost:3306/minions_db";
+    static final String MINIONS_DB_URL = "jdbc:mysql://localhost:3306/minions_db";
 
     public static final String COLUMN_LABEL_NAME = "name";
+
+    public static final String COLUMN_LABEL_MINION_COUNT = "count";
 }
