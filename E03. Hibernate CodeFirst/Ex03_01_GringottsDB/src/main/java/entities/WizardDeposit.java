6d2d47a72dcaed2d2c252e3bc123edf12c68f547
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-11-03 18:03:41 +0200

    Add files via upload

---
+++ E03. Hibernate CodeFirst/Ex03_01_GringottsDB/src/main/java/entities/WizardDeposit.java
@@ -0,0 +1,166 @@
+package entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+    @Entity
+    @Table(name = "wizard_deposits")
+    public class WizardDeposit {
+        @Id
+        @Column
+        private long id;
+
+        @Column(name = "first_name", length = 50)
+        private String firstName;
+
+        @Column(name = "last_name", length = 60)
+        private String lastName;
+
+        @Column(length = 1000)
+        private String notes;
+
+        @Column(nullable = false)
+        private int age;
+
+        @Column(name = "magic_wand_creator", length = 100)
+        private String magicWandCreator;
+
+        @Column(name = "magic_wand_size")
+        private int getMagicWandSize;
+
+        @Column(name = "deposit_group", length = 20)
+        private String depositGroup;
+
+        @Column(name = "deposit_start_date")
+        private LocalDate depositStartDate;
+
+        @Column(name = "deposit_amount")
+        private double depositAmount;
+
+        @Column(name = "deposit_interest")
+        private double depositInterest;
+
+        @Column(name = "deposit_charge")
+        private double depositCharge;
+
+        @Column(name = "deposit_expiration_date")
+        private LocalDate depositExpirationDate;
+
+        @Column(name = "is_deposit_expired")
+        private boolean isDepositExpired;
+
+        public WizardDeposit() {
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public String getNotes() {
+            return notes;
+        }
+
+        public void setNotes(String notes) {
+            this.notes = notes;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getMagicWandCreator() {
+            return magicWandCreator;
+        }
+
+        public void setMagicWandCreator(String magicWandCreator) {
+            this.magicWandCreator = magicWandCreator;
+        }
+
+        public int getGetMagicWandSize() {
+            return getMagicWandSize;
+        }
+
+        public void setGetMagicWandSize(int getMagicWandSize) {
+            this.getMagicWandSize = getMagicWandSize;
+        }
+
+        public String getDepositGroup() {
+            return depositGroup;
+        }
+
+        public void setDepositGroup(String depositGroup) {
+            this.depositGroup = depositGroup;
+        }
+
+        public LocalDate getDepositStartDate() {
+            return depositStartDate;
+        }
+
+        public void setDepositStartDate(LocalDate depositStartDate) {
+            this.depositStartDate = depositStartDate;
+        }
+
+        public double getDepositAmount() {
+            return depositAmount;
+        }
+
+        public void setDepositAmount(double depositAmount) {
+            this.depositAmount = depositAmount;
+        }
+
+        public double getDepositInterest() {
+            return depositInterest;
+        }
+
+        public void setDepositInterest(double depositInterest) {
+            this.depositInterest = depositInterest;
+        }
+
+        public double getDepositCharge() {
+            return depositCharge;
+        }
+
+        public void setDepositCharge(double depositCharge) {
+            this.depositCharge = depositCharge;
+        }
+
+        public LocalDate getDepositExpirationDate() {
+            return depositExpirationDate;
+        }
+
+        public void setDepositExpirationDate(LocalDate depositExpirationDate) {
+            this.depositExpirationDate = depositExpirationDate;
+        }
+
+        public boolean isDepositExpired() {
+            return isDepositExpired;
+        }
+
+        public void setDepositExpired(boolean depositExpired) {
+            isDepositExpired = depositExpired;
+        }
+    }
