58679798c2df703292ef6689d3b72a956b2e5a7f
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-11-06 16:48:35 +0200

    Add files via upload

---
+++ E03. Hibernate CodeFirst/Ex03_01_GringottsDB/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        Persistence.createEntityManagerFactory("gringotts")
+                .createEntityManager();
+    }
+}
